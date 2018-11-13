a586c37ceeda6a97e0290eac95774a4b3136ff42
Amy Ghotra <amy.ghotra8@gmail.com>
2018-11-13 17:48:29 -0500

    .

--- computeFare.py
+++ computeFare.py
@@ -1,38 +1,41 @@
-#program 43
+#this program will output ticket price based on info user provided
+
 
 def computeFare(zone , ticketType):
-    computeFare = ""
+    fare = ""
     if zone == 1 and ticketType == "peak":
-        computeFare = "6.75"
-        print("The fare is " , computeFare)
+        fare = "6.75"
     elif zone == 1 and ticketType == "off-peak":
-        computeFare = "5.00"
-        print("The fare is " , computeFare)
+        fare = "5.00"
     elif zone == 1 and ticketType == "senior":
-        computeFare = "3.35"
-        print("The fare is " , computeFare)
+        fare = "3.35"
     elif zone == 2 and ticketType == "peak":
-        computeFare = "7.50"
-        print("The fare is " , computeFare)
+        fare = "7.50"
     elif zone == 2 and ticketType == "off-peak":
-        computeFare = "5.75"
-        print("The fare is " , computeFare)
+        fare = "5.75"
     elif zone == 2 and ticketType == "senior":
-        computeFare = "3.75"
-        print("The fare is " , computeFare)
+        fare = "3.75"
     elif zone == 3 and ticketType == "peak":
-        computeFare = "9.25"
-        print("The fare is " , computeFare)
+        fare = "9.25"
     elif zone == 3 and ticketType == "off-peak":
-        computeFare = "7.00"
-        print("The fare is " , computeFare)
+        fare = "7.00"
     elif zone == 3 and ticketType == "senior":
-        computeFare = "4.50"
-        print("The fare is " , computeFare)
+        fare = "4.50"
+    elif zone > 3:
+        print("invalid zone number")
     else:
-        return computeFare
+        print("invalid input(s)")
     return computeFare
 
-x = int(input("input zone"))
-y = str(input("peak/off-peak/senior"))
-computeFare(x,y)
+def main():
+    z = int(input("Input zone number: "))           #get zone from user
+    t = input("Peak? Off-peak? Senior?")            #get more info from user
+    fare = computeFare(z,t)                         #invokes the computeFare function
+    if z >= 1 and z <= 3:                           #this function only takes zones from 1 - 3
+        print("The fare is: " , fare)
+    else:
+        print("invalid input(s)")
+
+if __name__ == "__main__":                          #call the function
+    main()
+
